4d16b7f3b2a8bde312fbcb0d277845edadb7302f
KorsK <kdong@my.chemeketa.edu>
2018-02-02 20:36:23 -0800

    new token

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var rawDataRoles = fs.readFileSync('./json/roles.json');
 const botInfo = JSON.parse(rawData);
 var rolesInfo = JSON.parse(rawDataRoles);
 
-bot.login('NDA2NzU4OTQ5MTc0NTA5NTc4.DU3nYw.JuyMFH9XdlTdXYFFstxYKP9epNg');
+//bot.login('NDA2NzU4OTQ5MTc0NTA5NTc4.DU3nYw.JuyMFH9XdlTdXYFFstxYKP9epNg');
+bot.login(process.env.BOT_TOKEN);
 
 bot.on('ready',(ready) => {
     bot.user.setActivity("in Korux's Basement");
