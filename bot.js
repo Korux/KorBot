9baaa07f681cf3b39c73707ba8e6d3aa42d3a07b
Korux <kdong@my.chemeketa.edu>
2018-02-17 20:17:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ var rolesInfo = JSON.parse(rawDataRoles);
 
 var botSpamControl = [];
 
-//bot.login(botToken.token);
+bot.login(botToken.token);
 
 bot.on('ready',(ready) => {
     bot.user.setActivity("Azur Lane");
@@ -495,5 +495,3 @@ bot.on('message',(message) => {
             });
         }
 });
-
-bot.login(process.env.BOT_TOKEN);
