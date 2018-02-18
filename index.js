877dd884393b620789d7ae894dd45956b1497e47
KorsK <kdong@my.chemeketa.edu>
2018-02-17 19:26:33 -0800

    heroku testing

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var rolesInfo = JSON.parse(rawDataRoles);
 var botSpamControl = [];
 
 bot.login(botToken.token);
+bot.login(process.env.BOT_TOKEN);
 
 bot.on('ready',(ready) => {
     bot.user.setActivity("Azur Lane");
